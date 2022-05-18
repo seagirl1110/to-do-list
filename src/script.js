cda1bc6e8e3ee4d31147516dfd74facc8257a6e8
Marina Elizarova <seagirl1110@gmail.com>
2022-05-18 17:53:52 +0300

    fix: fix

--- src/script.js
+++ src/script.js
@@ -1,3 +1,23 @@
 import './style.scss';
+import todoColl from './todoColl';
+import { renderTodoList } from './renderTodoList';
+import { addTodoItem } from './addTodoItem'
 
-const todoList = [];
+const form = document.querySelector('[data-todo="form"]');
+form.addEventListener('submit', (evt) => {
+    evt.preventDefault();
+})
+
+const input = form.querySelector('[data-todo="input"]');
+
+
+
+const btnAdd = form.querySelector('[data-todo="btn-add-item"]');
+btnAdd.addEventListener('click', () => {
+    addTodoItem(input)
+});
+
+const todoList = document.querySelector('[data-todo="list"]');
+renderTodoList(todoColl, todoList);
+
+export {todoList}
