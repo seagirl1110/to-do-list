548fe915f9372dbc26509c79b14e34b641029094
Marina Elizarova <seagirl1110@gmail.com>
2022-05-19 01:19:14 +0300

    feat: add btn-done

--- src/renderTodoList.js
+++ src/renderTodoList.js
@@ -1,11 +1,19 @@
-import { delTodoItem } from './store';
+import { doneTodoItem, delTodoItem } from './store';
 
 function renderTodoList(coll, list) {
     list.innerHTML = '';
     coll.forEach((element, index) => {
         const item = document.createElement('li');
-        item.classList.add('todo-list__item', 'todo-item');
+        item.classList.add('todo-list__item', 'todo-item', `todo-item--${element.status}`);
         item.innerHTML = `<span class="todo-item__name">${element.name}</span>`
+
+        const btnDone = document.createElement('button');
+        btnDone.classList.add('todo-item__btn-done');
+        btnDone.addEventListener('click', () => {
+            doneTodoItem(index);
+            renderTodoList(coll, list)
+        })
+        item.prepend(btnDone);
 
         const btnDel = document.createElement('button');
         btnDel.classList.add('todo-item__btn-del');
--- src/store.js
+++ src/store.js
@@ -18,9 +18,19 @@ const addTodoItem = (item) => {
     localStorage.setItem('todo', JSON.stringify(todoColl))
 }
 
+const doneTodoItem = (index) => {
+    if (todoColl[index].status === 'active') {
+        todoColl[index].status = 'done';
+    } else {
+        todoColl[index].status = 'active';
+    }
+
+    localStorage.setItem('todo', JSON.stringify(todoColl))
+}
+
 const delTodoItem = (index) => {
     todoColl.splice(index, 1);
     localStorage.setItem('todo', JSON.stringify(todoColl))
 }
 
-export {todoColl, addTodoItem, delTodoItem}
+export { todoColl, addTodoItem, doneTodoItem, delTodoItem }
