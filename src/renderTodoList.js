462da833f7934e293644b671ff5ac8bba9381e38
Marina Elizarova <seagirl1110@gmail.com>
2022-05-18 23:32:01 +0300

    feat: add btnDel

--- src/renderTodoList.js
+++ src/renderTodoList.js
@@ -1,11 +1,22 @@
+import { delTodoItem } from './store';
+
 function renderTodoList(coll, list) {
     list.innerHTML = '';
-    coll.forEach(element => {
+    coll.forEach((element, index) => {
         const item = document.createElement('li');
-        item.classList.add('todo-list__item');
-        item.textContent = element.name;
+        item.classList.add('todo-list__item', 'todo-item');
+        item.innerHTML = `<span class="todo-item__name">${element.name}</span>`
+
+        const btnDel = document.createElement('button');
+        btnDel.classList.add('todo-item__btn-del');
+        btnDel.addEventListener('click', () => {
+            delTodoItem(index);
+            renderTodoList(coll, list)
+        })
+        item.append(btnDel);
+
         list.append(item)
     });
 }
 
-export {renderTodoList}
+export { renderTodoList }
